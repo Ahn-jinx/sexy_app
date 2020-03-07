5df89ee01ae53490dc52033494a147341b00f982
unknown <zibbork@gmail.com>
2020-03-08 02:29:56 +0900

    Updates

---
+++ precache-manifest.03179a268b2b778c13189c9daf47818f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060bd5830ce653a2b356829cc94b08eb",
+    "url": "/sexy_app/index.html"
+  },
+  {
+    "revision": "eb33f156aff39ac41b61",
+    "url": "/sexy_app/static/css/main.dd291c8f.chunk.css"
+  },
+  {
+    "revision": "76965cda88315ef3c77d",
+    "url": "/sexy_app/static/js/2.a6508a44.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sexy_app/static/js/2.a6508a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb33f156aff39ac41b61",
+    "url": "/sexy_app/static/js/main.91535f7c.chunk.js"
+  },
+  {
+    "revision": "070aff7c2afd4dae882f",
+    "url": "/sexy_app/static/js/runtime-main.85a672f3.js"
+  }
+]);
